eae947bd0039e6ec34170139d0ccfff2da13b73b
286229825 <286229825@qq.com>
2018-08-29 22:34:35 +0800

    提交

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.cfgbeans;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
+
+@Configuration
+public class ConfigBean //boot -->spring   applicationContext.xml --- @Configuration配置   ConfigBean = applicationContext.xml
+{ 
+	@Bean
+	@LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
+	
+	@Bean
+	public IRule myRule()
+	{
+		//为达到的目的，用我们重新选择的负载均衡算法来替代默认的轮询。
+		//只需要new相应的对象即可
+		//return new RoundRobinRule();
+		//return new RandomRule();
+		return new RetryRule();
+	}
+}
+
+//@Bean
+//public UserServcie getUserServcie()
+//{
+//	return new UserServcieImpl();
+//}
+// applicationContext.xml == ConfigBean(@Configuration)
+//<bean id="userServcie" class="com.atguigu.tmall.UserServiceImpl">
---
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进Eureka中
+//@EnableDiscoveryClient //如果服务注册中心不是用的Eureka，则使用这个注解来代替@EnableEurekaClient，用于服务发现
+public class DeptProvider8001_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptProvider8001_App.class, args);
+	}
+}
